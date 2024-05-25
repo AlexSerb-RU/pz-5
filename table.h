ecccabe0527469a8d34bf0120f322a477fc3b538
AlexSerb <visualstud.c0rp@mail.ru>
2024-05-25 23:47:01 +0700

    фикс поиска в storage

--- table.h
+++ table.h
@@ -63,7 +63,6 @@ struct storage
 {
    UCHAR n = 0;
    sortmode srtm = QUICK; // sort mode
-   //searchmode srchm = BINARY; // search mode
    product *res[N]{ };
 
    inline bool IsFull( ) { return N == n; }
@@ -72,23 +71,11 @@ struct storage
    bool Search( UINT key, UCHAR *i )
    {
       if ( IsEmpty( ) ) return false;
-      /*switch ( srchm )
-      {
-         case BINARY:
-            for ( ; l < r && key != res[m]->key; m = (l + r) / 2 )
-               key >= res[m]->key ? l = m + 1 : r = m;
-            break;
-         case LINEAR:
-            for ( UCHAR b = 0 ; b < n && res[b]->key < key; ++b )
-               m = b;
-      }*/
-
-      // Интересная идея попытаться использовать бинарный поиск в неупорядоченной таблице, что могло пойти не так...
-      /*UCHAR l = 0, r = n - 1, m = (l + r) / 2;
-      for ( ; l < r && key != res[m]->key; m = (l + r) / 2 )
-         key >= res[m]->key ? l = m + 1: r = m;
-      *i = m + (res[m]->key < key);
-      return key == res[m]->key;*/
+      UCHAR b = 0;
+      for ( ; b + 1 < n && res[b]->key != key; )
+         b++;
+      *i = b;
+      return res[b]->key == key;
    }
 
    UCHAR Add( product *r )
@@ -109,21 +96,19 @@ struct storage
             break;
          case SHELL:
             UCHAR l = 0;
-            UCHAR *h = new UCHAR[29];
-            for ( h[0] = 1; h[l] <= n / 9 ;
-               l++, h[l] = 2 * h[l - 1] + 1 );
+            UCHAR *gap = new UCHAR[29];
+            for ( gap[l] = 0; gap[l] <= n / 9 ; l = l + 1, gap[l] = 3 * gap[l - 1] + 1 );
             for ( short m = l; m >= 0; m-- )
-               for ( UCHAR k = 0; k < h[m]; k++ )
-                  for ( UCHAR i = h[m] + k; i <= n - 1; i += h[m] )
-                  {
-                     short j = i - h[m];
-                     while ( j >= 0 && res[i]->key > res[j]->key )
-                     {
-                        res[j + h[m]] = res[j];
-                        j = j - h[m];
-                     }
-                     res[j + h[m]] = res[i];
-                  }
+            {
+               for ( int i = gap[m]; i < n; i += 1 )
+               {
+                  product *x = res[i];
+                  short j = i;
+                  for ( ; j >= gap[m] && res[j - gap[m]]->key > x->key; j -= gap[m] )
+                     res[j] = res[j - gap[m]];
+                  res[j] = x;
+               }
+            }
       }
    }
 };
